c03ea97e6617745a3c2f12e77d6e0982351f409b
Joshua Samuel <sauhsoj@amazon.com>
2020-11-08 13:02:20 +1100

    Amazon Porting Assistant for .NET

--- TeslaLogger/Properties/AssemblyInfo.cs
+++ TeslaLogger/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("TeslaLogger")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TeslaLogger")]
-[assembly: AssemblyCopyright("Copyright © 2018 by Christian Pogea")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
 // für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
 // COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
@@ -21,18 +9,5 @@ using System.Runtime.CompilerServices;
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("9932f717-1bee-4d0f-b5b0-fb8665ffe647")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.45.8.0")]
-[assembly: AssemblyFileVersion("1.45.8.0")]
 
 [assembly: InternalsVisibleTo("UnitTestsTeslalogger")]
